0c6eaa4c19d61647d560e515d94e62655faacabb
afghl <airport.0930@gmail.com>
2017-01-08 18:59:13 +0800

    add components and styling

--- containers/App.js
+++ containers/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        helloworld
+        <Header />
+        <main id={"main"}>
+          main section
+        </main>
+        <Footer />
       </div>
     )
   }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import 'babel-polyfill'
+import './sass/main.scss'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
