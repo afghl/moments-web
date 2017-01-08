c0a0aa1f8467e76bde5ed87f65a38d62bfd8ae2b
afghl <airport.0930@gmail.com>
2017-01-08 14:58:47 +0800

    add helloworld react framework

--- store/configureStore.js
+++ store/configureStore.js
@@ -2,11 +2,10 @@ import { applyMiddleware, createStore } from 'redux'
 import rootReducer from '../reducers/index'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
-import api from '../middleware/api'
 
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk, api, logger())
+    applyMiddleware(thunk, logger())
   )
 }
