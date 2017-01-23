abfe8741385bc06c8cac4f8e581a37b5b82e8910
afghl <airport.0930@gmail.com>
2017-01-23 11:49:08 +0800

    display comment list

--- containers/CommentList.js
+++ containers/CommentList.js
@@ -3,31 +3,49 @@ import { connect } from 'react-redux'
 import List from '../components/List'
 import * as status from '../reducers/paginate'
 import filter from 'lodash/filter'
+import isEmpty from 'lodash/isEmpty'
 
 const mapStateToProps = (state, ownProps) => {
   const entities = state.entities.comments
   const ids = ownProps.moment.comments
   const comments = ids.map(id => entities[id])
 
-  const likes = filter(comments, c => c.type == 1)
-  const talks = filter(comments, c => c.type == 2)
+  const talks = filter(comments, c => c.type == 1)
+  const likes = filter(comments, c => c.type == 2)
   return { likes, talks }
 }
 
 class CommentList extends Component {
 
   renderLike(like) {
-    const { userAvatar } = like
-
     return (
       <li className="likes-item">
-        <img src={userAvatar}/>
+        <img src={like.userAvatar}/>
+      </li>
+    )
+  }
+
+  renderTalk(talk) {
+    console.log(talk);
+
+    return (
+      <li className="talks-item">
+        <p>
+          <span className="replier">
+            {talk.userName}
+          </span>
+          { !isEmpty(talk.otherName) ?
+              (<span className="other-replier"> 回复 {talk.otherName} </span>) : ('')
+          }
+          ：
+          {talk.body}
+        </p>
       </li>
     )
   }
 
   render() {
-    const { renderLike } = this
+    const { renderLike, renderTalk } = this
     const { likes, talks } = this.props
 
     if (likes.length == 0 && talks.length == 0)
@@ -42,8 +60,11 @@ class CommentList extends Component {
           items={likes}
           className={"comments-likes"}
         />
-
-
+        <List
+          renderItem={renderTalk}
+          items={talks}
+          className={"comments-talks"}
+        />
       </div>
     )
   }
