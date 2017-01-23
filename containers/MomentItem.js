fe288b0dc05997a88f037fda7b1711a1df675468
afghl <airport.0930@gmail.com>
2017-01-23 15:00:26 +0800

    add icons and use iconfont

--- containers/MomentItem.js
+++ containers/MomentItem.js
@@ -16,6 +16,12 @@ class MomentItem extends Component {
           <p className="userName">{user.name}</p>
           <p className="body">{body}</p>
           <p className="timestamp">{time}</p>
+          <div className="oprations">
+            <ul>
+              <li><span className="icon icon-reply"></span></li>
+              <li><span className="icon icon-like">like</span></li>
+            </ul>
+          </div>
           <CommentList moment={this.props.moment}/>
         </div>
         <div className="border"></div>
