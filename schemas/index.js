c1b87d0ea3a56edb16125d08057c2c3d4ab8ee63
afghl <airport.0930@gmail.com>
2017-01-23 10:28:35 +0800

    upadte schema so that it can parse comments into entities

--- schemas/index.js
+++ schemas/index.js
@@ -2,13 +2,19 @@ import { Schema, arrayOf, normalize } from 'normalizr'
 
 const userSchema = new Schema('users', { idAttribute: 'id' })
 const momentSchema = new Schema('moments', { idAttribute: 'id' })
+const commentSchema = new Schema('comments', { idAttribute: 'id' })
 
-momentSchema.define({ user: arrayOf(userSchema)})
+momentSchema.define({
+  user: arrayOf(userSchema),
+  comments: arrayOf(commentSchema)
+})
 
 // Schemas for Moments API responses.
 export default {
   USER: userSchema,
   USER_ARRAY: arrayOf(userSchema),
   MOMENT: momentSchema,
-  MOMENT_ARRAY: arrayOf(momentSchema)
+  MOMENT_ARRAY: arrayOf(momentSchema),
+  COMMENT: commentSchema,
+  COMMENT_ARRAY: arrayOf(commentSchema)
 }
