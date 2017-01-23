d371e22d035cb23805f58f90a522bd0bc5abf197
afghl <airport.0930@gmail.com>
2017-01-23 16:10:07 +0800

    add like icon

--- containers/MomentItem.js
+++ containers/MomentItem.js
@@ -16,12 +16,10 @@ class MomentItem extends Component {
           <p className="userName">{user.name}</p>
           <p className="body">{body}</p>
           <p className="timestamp">{time}</p>
-          <div className="oprations">
-            <ul>
-              <li><span className="icon icon-reply"></span></li>
-              <li><span className="icon icon-like">like</span></li>
-            </ul>
-          </div>
+          <ul className="oprations">
+            <li><span className="icon icon-reply"></span></li>
+            <li className="like-container"><span className="icon icon-like"></span></li>
+          </ul>
           <CommentList moment={this.props.moment}/>
         </div>
         <div className="border"></div>
